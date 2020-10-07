c641fd0f7025e127761eda1d55c925c35b2a05af
Joel Esteban Diaz Arevalo <jodiaza@unal.edu.co>
2020-10-06 19:20:56 -0500

    Archivos Java agregados

--- src/java/Logica/SimplyLinkedList.java
+++ src/java/Logica/SimplyLinkedList.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Logica;
 
-/**
- *
- * @author Estudiante
- */
-public class SimplyLinkedList {
+public class SimplyLinkedList<T> extends List<T> {
+
+    Node<T> first;
+    Integer length;
+
+    public SimplyLinkedList() {
+        this.length = 0;
+        this.first = null;
+    }
+    @Override
+    void insert(T value) {
+        if (this.length == 0) {
+            this.first = new Node<T>(value);
+            this.length++;
+            return;
+        }
+        Node<T> aux = this.first;
+        while (aux.next != null) {
+            aux = aux.next;
+        }
+        aux.next = new Node<T>(value);
+        this.length++;
+    }
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<T> aux = this.first;
+        while (aux.next != null){
+            sb.append(aux.value + ", ");
+            aux = aux.next;
+        }
+        sb.append(aux.value);
+        sb.append("]");
+        return sb.toString();
+    }
     
 }
