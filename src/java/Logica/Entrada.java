c0698d846c0ae53c6186224c5b2873ec9deebe83
Oscar <osurregor@unal.edu.co>
2020-10-17 17:59:04 -0500

    Clase Entrada

---
+++ src/java/Logica/Entrada.java
@@ -0,0 +1,30 @@
+
+
+package Logica;
+
+public class Entrada {
+
+    public String español;
+    public String bue;
+
+    public Entrada(String español, String bue) {
+        this.español = español;
+        this.bue = bue;
+    }
+
+    public String getEspañol() {
+        return español;
+    }
+
+    public void setEspañol(String español) {
+        this.español = español;
+    }
+
+    public String getBue() {
+        return bue;
+    }
+
+    public void setBue(String bue) {
+        this.bue = bue;
+    }
+}
