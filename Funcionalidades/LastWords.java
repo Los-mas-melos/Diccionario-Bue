be4dd41c9e177ea5daaea6ddce8dcb1042b37c32
Sonny N <snino@unal.edu.co>
2020-10-17 21:04:30 -0500

    Paquetes para estructuras de datos y las pruebas que se vayan haciendo.

---
+++ Funcionalidades/LastWords.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import com.company.ArrayQueue;
+
+public class LastWords
+{
+    private ArrayQueue<String> queue;
+
+    public LastWords()
+    {
+        queue = new ArrayQueue<String>();
+    }
+
+    public void addWord(String word){
+        if(queue.full())
+            queue.dequeue();
+
+        queue.enqueue(word);
+    }
+
+    public ArrayQueue<String> getArrayQueue(){
+        return queue;
+    }
+
+    public void setArrayQueue(ArrayQueue queue){
+        this.queue = queue;
+    }
+
+}
